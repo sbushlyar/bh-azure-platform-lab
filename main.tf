8be21613f81919d9a8a92c219c210f8dd60f73fb
Sergey Bushlyar <sbushlyar@gmail.com>
2026-09-20 08:17:08 -0400

    Add application subnet

--- main.tf
+++ main.tf
@@ -20,3 +20,9 @@ resource "azurerm_virtual_network" "lab" {
   location            = azurerm_resource_group.lab.location
   resource_group_name = azurerm_resource_group.lab.name
 }
+resource "azurerm_subnet" "lab" {
+  name                 = "snet-app"
+  resource_group_name  = azurerm_resource_group.lab.name
+  virtual_network_name = azurerm_virtual_network.lab.name
+  address_prefixes     = ["10.10.1.0/24"]
+}
