a9a2c958f8026441748d3664aad8deba92e2d6cb
Sergey Bushlyar <sbushlyar@gmail.com>
2026-09-20 07:50:11 -0400

    Add Azure virtual network

--- main.tf
+++ main.tf
@@ -14,3 +14,9 @@ resource "azurerm_resource_group" "lab" {
   name     = "rg-bh-platform-lab"
   location = "East US"
 }
+resource "azurerm_virtual_network" "lab" {
+  name                = "vnet-bh-platform-lab"
+  address_space       = ["10.10.0.0/16"]
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+}
