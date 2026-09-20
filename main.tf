265b89824e27164eeaa8d4a671dba3f9d803a753
Sergey Bushlyar <sbushlyar@gmail.com>
2026-09-20 08:47:20 -0400

    Add network security group

--- main.tf
+++ main.tf
@@ -26,3 +26,8 @@ resource "azurerm_subnet" "lab" {
   virtual_network_name = azurerm_virtual_network.lab.name
   address_prefixes     = ["10.10.1.0/24"]
 }
+resource "azurerm_network_security_group" "lab" {
+  name                = "nsg-app"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+}
